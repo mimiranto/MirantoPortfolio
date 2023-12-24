8e721426458e2d815870279e221338990288b492
mimiranto <mirantorakotobe@icloud.com>
2023-12-24 22:28:31 +0100

    Presentation

--- src/component/presentation/index.js
+++ src/component/presentation/index.js
@@ -4,6 +4,12 @@ function Presentation(){
 
     return(
         <div className='container'>
+           <img src='' alt='Image'/> 
+
+           <div className='textecontainer'>
+                <h1>Bienvenue sur mon portfolio , Je m'appelle Miranto Rakotobe</h1>
+                <h3>Étant étudiant à l'IPSSI en Prépa Bachelor Dév & Cybersécurité, je suis actuellement en quête d'un stage de 2 mois en rapport avec la cybersécurité. Passionné par les enjeux de protection des systèmes informatiques, je suis motivé(e) à mettre en pratique les connaissances acquises au cours de ma formation et à contribuer activement à renforcer la sécurité numérique au sein d'entreprises. </h3>
+           </div>
 
         </div>
     )
