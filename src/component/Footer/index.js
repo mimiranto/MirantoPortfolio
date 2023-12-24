a1d54f7c451f88b5b89e6b2edfe662fb28e231e4
mimiranto <mirantorakotobe@icloud.com>
2023-12-24 23:58:02 +0100

    footer

--- src/component/Footer/index.js
+++ src/component/Footer/index.js
@@ -1,11 +1,23 @@
 import './index.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 function Footer(){
  
     return(
-        <header className='myheader'>
-            
-        </header>
+        <div className='footer'>
+            <div className='texte'>
+                <h3><a href=''></a></h3>
+                <h3><a href=''></a></h3>
+            </div>
+            <div className='texte2'>
+                <h3>© Designed by Miranto</h3>
+            </div>
+            <div className='logo'>
+                <a href=''><FontAwesomeIcon icon={faLinkedin} size="lg" style={{ color: "#ffffff" }} /></a>
+                <a href=''><FontAwesomeIcon icon={faGithub} size="lg" style={{ color: "#ffffff" }} /></a>
+            </div>
+        </div>
 
     )
 
