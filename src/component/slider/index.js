edc302db8b13c3ea6e21be6f232d6b48182f3c9f
mimiranto <134143539+mimiranto@users.noreply.github.com>
2023-12-31 01:32:42 +0100

    slider responsive parfait

--- src/component/slider/index.js
+++ src/component/slider/index.js
@@ -8,7 +8,7 @@ const Slider = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const showSlide = (index) => {
-    const newPosition = -index * 25 + '%';
+    const newPosition = -index * 50 + '%';
     sliderRef.current.style.transform = 'translateX(' + newPosition + ')';
   };
 
@@ -22,30 +22,27 @@ const Slider = () => {
     showSlide(currentIndex);
   };
 
-  const slideCount = 3; // Remplacez par le nombre total de slides
+  const slideCount = 2; // Nombre total d'images
 
   return (
     <div className='slider'>
       <ul className="trends__content" ref={sliderRef}>
         <li className="trends__card">
           <a href='https://github.com/mimiranto/TooLate' target='blank'>
-          <img src={img1} alt="Too Late" /> 
-          <h1>Projet Unity : Too Late</h1>
+            <img src={img1} alt="Too Late" />
+            <h1>Projet Unity : Too Late</h1>
           </a>
         </li>
-        <li className="trends__card">
+        <li className="trends__card" id='img2'>
           <a href='https://little-italia.vercel.app/' target='blank'>
-          <img src={img2} alt="Little Italia" />
-          <h1>Projet Site : Little Italia</h1>
+            <img src={img2} alt="Little Italia" />
+            <h1>Projet Site : Little Italia</h1>
           </a>
-        </li>
-        <li className="trends__card">
-          <img src="" alt="" />
         </li>
       </ul>
 
-      <button className='arrow left' onClick={prevSlide}>GAUCHE</button>
-      <button className='arrow right' onClick={nextSlide}>DROITE</button>
+      <button className='arrow left' onClick={prevSlide}>{'<'}</button>
+      <button className='arrow right' onClick={nextSlide}>{'>'}</button>
     </div>
   );
 };
